7bf49fe8e1aa6eeb56c857ced285d747a25c710f
Bailiang <18762590320@163.com>
2021-02-13 12:24:43 +0800

    更新2

--- configs.js
+++ configs.js
@@ -1,6 +1,6 @@
 YL.static = {
   /** “关于”信息 */
-  softwareName: 'Akilarの糖果屋', //网站名。请在此处填写您自己的网站名，如王小明的博客。
+  softwareName: 'Acacia\'s Blog', //网站名。请在此处填写您自己的网站名，如王小明的博客。
   version: "1.0.0", // 网站版本号
   iconBtnStart: 'html5', //主图标
   author: 'Akilar',//作者
